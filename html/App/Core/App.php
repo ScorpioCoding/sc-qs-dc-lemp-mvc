653831b4018f02539f33605a3ffcb5e07ba7b743
denOldTimer <kribo@outlook.com>
2023-01-08 00:49:42 +0100

    refactor code

--- html/App/Core/App.php
+++ html/App/Core/App.php
@@ -32,16 +32,8 @@ class App
       $router->addRoute($route, $params);
     };
 
-
-
-    //echo '<br /> PATH_HOST  :: ' . $_SERVER['HTTP_HOST'];
-    //echo '<br /> REQUEST_URI  :: ' . $_SERVER['REQUEST_URI'];
-
-
-
     //PARSING URL
     $tokens = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES);
-    //var_dump($tokens);
 
     //DISPATCH
     $router->dispatch($tokens);
