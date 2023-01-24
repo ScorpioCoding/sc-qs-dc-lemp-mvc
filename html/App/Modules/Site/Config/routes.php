6e5df287c6db4a343c75a97b046627704e9d72d4
denOldTimer <kribo@outlook.com>
2023-01-24 16:24:31 +0100

    refactoring

--- html/App/Modules/Site/Config/routes.php
+++ html/App/Modules/Site/Config/routes.php
@@ -5,4 +5,5 @@ return (object) array(
   ''    => ['lang' => 'en', 'module' => 'site', 'namespace' => 'App\Modules\Site\Controllers', 'controller' => 'home', 'action' => 'index'],
   '/'   => ['lang' => 'en', 'module' => 'site', 'namespace' => 'App\Modules\Site\Controllers', 'controller' => 'home', 'action' => 'index'],
 
+
 );
